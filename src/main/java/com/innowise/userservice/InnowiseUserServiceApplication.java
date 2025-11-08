b203ade59c08560ef50f0c65657d0b62e0a97fd6
Matvey <m5221211m@gmail.com>
2025-11-08 11:14:14 +0300

    first commit

---
+++ src/main/java/com/innowise/userservice/InnowiseUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowiseUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnowiseUserServiceApplication.class, args);
+    }
+
+}
