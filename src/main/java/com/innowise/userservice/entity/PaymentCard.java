9ac5e995de64614c46b1122ffd87bb1c1ff2d2b0
Matvey <m5221211m@gmail.com>
2025-11-08 13:03:24 +0300

    feat: add JPA entities

---
+++ src/main/java/com/innowise/userservice/entity/PaymentCard.java
@@ -0,0 +1,36 @@
+package com.innowise.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "payment_cards")
+public class PaymentCard extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "number", nullable = false, length = 64)
+    private String number;
+
+    @Column(name = "holder", nullable = false, length = 128)
+    private String holder;
+
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+    @ColumnDefault("true")
+    @Column(name = "active")
+    private Boolean active;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
