078a839522cb29ef2952d2f2165b1d52d837498c
Matvey <m5221211m@gmail.com>
2025-11-12 10:46:36 +0300

    feat: add dtos and mappers

---
+++ src/main/java/com/innowise/userservice/dto/request/PaymentCardUpdateDto.java
@@ -0,0 +1,21 @@
+package com.innowise.userservice.dto.request;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class PaymentCardUpdateDto {
+
+    @NotNull(message = "Card id is required")
+    private Long id;
+
+    @Size(min = 12, max = 64, message = "Card number length must be between 12 and 64 characters")
+    private String number;
+
+    @Future(message = "Expiration date must be in the future")
+    private LocalDate expirationDate;
+}
