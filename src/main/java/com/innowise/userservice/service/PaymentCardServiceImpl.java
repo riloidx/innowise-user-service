76c6fee8c199584e157d7530f08adf1ba0122410
Matvey <m5221211m@gmail.com>
2025-11-12 22:54:20 +0300

    feat: change service layer structure

--- src/main/java/com/innowise/userservice/service/PaymentCardServiceImpl.java
+++ src/main/java/com/innowise/userservice/service/PaymentCardServiceImpl.java
@@ -1,4 +1,4 @@
-package com.innowise.userservice.service.impl;
+package com.innowise.userservice.service;
 
 import com.innowise.userservice.config.CardProperties;
 import com.innowise.userservice.dto.request.PaymentCardCreateDto;
@@ -6,28 +6,28 @@ import com.innowise.userservice.dto.request.PaymentCardUpdateDto;
 import com.innowise.userservice.dto.response.PaymentCardResponseDto;
 import com.innowise.userservice.entity.PaymentCard;
 import com.innowise.userservice.entity.User;
-import com.innowise.userservice.exception.BadRequestException;
-import com.innowise.userservice.exception.PaymentCardAlreadyExistsException;
-import com.innowise.userservice.exception.PaymentCardLimitExceededException;
 import com.innowise.userservice.mapper.PaymentCardMapper;
 import com.innowise.userservice.repository.PaymentCardRepository;
-import com.innowise.userservice.service.api.PaymentCardCommandService;
-import com.innowise.userservice.service.api.PaymentCardDataAccessService;
-import com.innowise.userservice.service.api.UserDataAccessService;
+import com.innowise.userservice.specification.PaymentCardSpecification;
 import com.innowise.userservice.util.ValidationUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-public class PaymentCardCommandServiceImpl implements PaymentCardCommandService {
+public class PaymentCardServiceImpl implements PaymentCardService {
 
     private final PaymentCardRepository paymentCardRepo;
-    private final PaymentCardDataAccessService paymentCardDataAccessService;
     private final PaymentCardMapper mapper;
     private final ValidationUtil validationUtil;
-    private final UserDataAccessService userDataAccessService;
+    private final UserService userService;
     private final CardProperties cardProperties;
 
     @Override
@@ -66,6 +66,61 @@ public class PaymentCardCommandServiceImpl implements PaymentCardCommandService 
         return mapper.toDto(card);
     }
 
+    @Override
+    public PaymentCard findById(long id) {
+        return paymentCardRepo.findById(id).
+                orElseThrow(() -> new PaymentCardNotFoundException("id", String.valueOf(id)));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public PaymentCardResponseDto findDtoById(long id) {
+        return mapper.toDto(findById(id));
+    }
+
+    @Override
+    public Page<PaymentCardResponseDto> findAll(Boolean active,
+                                                LocalDate expiresAfter,
+                                                LocalDate expiresBefore,
+                                                Pageable pageable) {
+        Specification<PaymentCard> spec = configureSpecification(active,
+                expiresAfter,
+                expiresBefore);
+
+        Page<PaymentCard> paymentCards = paymentCardRepo.findAll(spec, pageable);
+
+        return mapper.toDto(paymentCards);
+    }
+
+    @Override
+    public List<PaymentCardResponseDto> findAllByUserId(long userId) {
+        List<PaymentCard> paymentCards = paymentCardRepo.findAllByUserId(userId);
+
+        return mapper.toDto(paymentCards);
+    }
+
+    private Specification<PaymentCard> configureSpecification(Boolean active,
+                                                              LocalDate expiresAfter,
+                                                              LocalDate expiresBefore) {
+
+        Specification<PaymentCard> spec = Specification.unrestricted();
+
+        if (active != null) {
+            spec = spec.and(PaymentCardSpecification.isActive(active));
+        }
+
+        if (expiresAfter != null) {
+            spec = spec.and(PaymentCardSpecification.expiresAfter(expiresAfter));
+        }
+
+        if (expiresBefore != null) {
+            spec = spec.and(PaymentCardSpecification.expiresBefore(expiresBefore));
+        }
+
+        return spec;
+
+    }
+
     private void checkCardNumberNotTaken(String cardNumber) {
         paymentCardRepo.findByNumber(cardNumber)
                 .ifPresent(card -> {
@@ -76,7 +131,7 @@ public class PaymentCardCommandServiceImpl implements PaymentCardCommandService 
     private PaymentCard getValidatedCardForCreation(PaymentCardCreateDto paymentCardCreateDto) {
         checkCardNumberNotTaken(paymentCardCreateDto.getNumber());
 
-        User user = userDataAccessService.findById(paymentCardCreateDto.getUserId());
+        User user = userService.findById(paymentCardCreateDto.getUserId());
 
         if (user.getPaymentCards().size() >= cardProperties.getMaxLimit()) {
             throw new PaymentCardLimitExceededException("Maximum number of cards (" +
@@ -97,7 +152,7 @@ public class PaymentCardCommandServiceImpl implements PaymentCardCommandService 
     private PaymentCard getValidatedCardForUpdate(long id, PaymentCardUpdateDto dto) {
         validationUtil.validateMatchingIds(id, dto.getId());
 
-        PaymentCard existingCard = paymentCardDataAccessService.findById(id);
+        PaymentCard existingCard = findById(id);
 
         if (!existingCard.getNumber().equals(dto.getNumber())) {
             checkCardNumberNotTaken(dto.getNumber());
@@ -107,7 +162,7 @@ public class PaymentCardCommandServiceImpl implements PaymentCardCommandService 
     }
 
     private PaymentCard getValidatedCardForChangingStatus(long id, boolean active) {
-        PaymentCard card = paymentCardDataAccessService.findById(id);
+        PaymentCard card = findById(id);
 
         if (active == card.getActive()) {
             throw new BadRequestException("Card with id=" + id + " have status=" + (active ? "active" : "inactive"));
@@ -117,4 +172,6 @@ public class PaymentCardCommandServiceImpl implements PaymentCardCommandService 
 
         return card;
     }
+
+
 }
