e5910b723d7a936ad93dd3ba2405a70096fe0688
Matvey <m5221211m@gmail.com>
2025-11-16 00:42:40 +0300

    refactor: delete annotations

--- src/test/java/com/innowise/userservice/integration/BaseIntegrationTest.java
+++ src/test/java/com/innowise/userservice/integration/BaseIntegrationTest.java
@@ -4,17 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers(disabledWithoutDocker = true)
 @SpringBootTest
-@ActiveProfiles("test")
 @AutoConfigureMockMvc
+@Testcontainers(disabledWithoutDocker = true)
 public abstract class BaseIntegrationTest {
 
     @Container
